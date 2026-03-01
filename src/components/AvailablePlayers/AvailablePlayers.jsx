494f803abe79999b6724582548cc06841ed96905
ALAMIN761740 <abdullahalamin761740@gmail.com>
2026-03-02 02:49:40 +0600

    add fallback image handling for player cards

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -1,6 +1,6 @@
 import userImg from '../../assets/user-1.png';
 import flagImg from '../../assets/report-1.png';
-
+import unknownImg from '../../assets/unknown-image.png';
 
 
 const AvailablePlayers = ({ availablePlayers }) => {
@@ -14,13 +14,19 @@ const AvailablePlayers = ({ availablePlayers }) => {
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-300 mx-auto">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-center items-center gap-3 max-w-300 mx-auto py-10">
       {availablePlayers.map((player) => (
-        <div key={player.id} className="card p-4 bg-base-100 w-96 shadow-sm">
+        <div key={player.id} className="card p-4 bg-base-100 w-full max-w-sm mx-auto shadow-sm">
           <figure>
-            <img className="h-64 w-full object-cover"
-              src={player.image}
-              alt={player.name} />
+              <img
+                className="h-64 w-full object-cover"
+                src={player.image || unknownImg}
+                alt={player.name}
+                onError={(e) => {
+                  e.target.onerror = null; 
+                  e.target.src = unknownImg;
+                }}
+              />
           </figure>
           <div className="mt-4">
 
@@ -29,7 +35,7 @@ const AvailablePlayers = ({ availablePlayers }) => {
               <h2 className="ml-2 font-bold">{player.name}</h2>
             </div>
 
-            <div className='flex justify-between mt-2 mb-2'>
+            <div className='flex justify-between mt-2 mb-2 border-b-2 pb-4 border-gray-300'>
               <div className='flex items-center'>
                 <img src={flagImg} alt="Flag" className="w-4 h-4 inline-block " />
                 <h2 className="ml-2 font-bold">{player.country}</h2>
@@ -42,13 +48,13 @@ const AvailablePlayers = ({ availablePlayers }) => {
                 <h2>Rating</h2>
                 <h2>{player.rating}</h2>
               </div>
-              <div className='flex justify-between font-bold mt-2 mb-2'>
-                <h2 className='outline p-1'>{player.battingStyle}</h2>
-                <h2 className='outline p-1'>{player.bowlingStyle}</h2>
+              <div className='flex justify-between  rounded mt-2 mb-2'>
+                <button className='btn btn-sm btn-outline font-bold'>{player.battingStyle}</button>
+                <button className='btn btn-sm btn-outline'>{player.bowlingStyle}</button>
               </div>
-              <div className='flex justify-between mt-2 mb-2'>
-                <h2>${player.price}</h2>
-                <button className="btn btn-primary">Buy Now</button>
+              <div className='flex justify-between mt-2 mb-2 items-center'>
+                <h2 className="font-bold text-xl items-center">Price:${player.price}</h2>
+                <button className="btn btn-sm btn-outline">Buy Now</button>
               </div>
             </div>
             
