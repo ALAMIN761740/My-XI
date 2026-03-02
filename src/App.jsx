93dda6f63c6ef11045900a660657944cc25517c8
ALAMIN761740 <abdullahalamin761740@gmail.com>
2026-03-03 01:01:21 +0600

    Added toggle state to switch between Available and Selected Players

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import AvailablePlayers from "./components/AvailablePlayers/AvailablePlayers";
 import Navbar from "./components/Navbar/Navbar";
 import SelectedPlayers from "./components/SelectedPlayers/SelectedPlayers";
 
+
 function App() {
+  const [toggle, setToggle] = useState(true);
+
+
   const [availablePlayers, setAvailablePlayers] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -21,13 +25,36 @@ function App() {
     <>
       <Navbar />
 
-      {loading ? (
-        <div className="text-center py-10"><span className="loading loading-spinner text-info"></span></div>
-      ) : (
-        <AvailablePlayers availablePlayers={availablePlayers} />
-      )}
 
-      <SelectedPlayers />
+
+      <div className="max-w-300 mx-auto justify-between items-center flex ">
+        <h1 className="text-2xl font-bold text-center ">{toggle ? "Available Players" : "Selected Players"}</h1>
+
+        <div className="flex font-bold">   
+          <button onClick={() => setToggle(true)} className={`py-3 px-4 border border-gray-400 rounded-l-2xl border-r-0 ${toggle ? 'bg-[#E7FE29]' : ''}`}>Available Players</button>
+          <button onClick={() => setToggle(false)} className={`py-3 px-4 border border-gray-400 rounded-r-2xl border-l-0 ${!toggle ? 'bg-[#E7FE29]' : ''}`}>Selected Players <span>{0}</span></button>
+        </div>
+      </div>
+      
+      {
+        toggle === true ? (
+          loading ? (
+            <div className="text-center py-10"><span className="loading loading-spinner text-info"></span></div>
+          ) : (
+            <AvailablePlayers availablePlayers={availablePlayers} />
+          )
+        ) : (
+          <SelectedPlayers />
+        )
+      }
+
+
+
+     
+
+
+
+      
     </>
   );
 }
--- src/components/SelectedPlayers/SelectedPlayers.jsx
+++ src/components/SelectedPlayers/SelectedPlayers.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SelectedPlayers = () => {
     return (
-        <div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-center items-center gap-3 max-w-300 mx-auto py-10">
             <h1>Selected Players</h1>
         </div>
     );
